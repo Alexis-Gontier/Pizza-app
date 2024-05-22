d7f225301217ab76ff8b2bb5119a3370285441c5
Alexis G <116964181+Alexis-Gontier@users.noreply.github.com>
2024-05-22 05:41:57 +0200

    Update firebaseConfig.js

--- app/db/firebaseConfig.js
+++ app/db/firebaseConfig.js
@@ -2,12 +2,12 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCpnI6T7i7fn-NVCM4KWo4nXBg82dmNcJs",
-  authDomain: "app-pizza-a4508.firebaseapp.com",
-  projectId: "app-pizza-a4508",
-  storageBucket: "app-pizza-a4508.appspot.com",
-  messagingSenderId: "544032684521",
-  appId: "1:544032684521:web:6d80efc5c1ab489885b378"
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: ""
 };
 
 export const app = initializeApp(firebaseConfig);
